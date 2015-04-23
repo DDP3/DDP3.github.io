6bf8b47425fd0511fb5dae89c7444609ef3674b9
Dustin Diep <dustindiep0@gmail.com>
2015-04-22 23:14:14 -0700

    Form test working
    Very basic functionality. You can input into the form and it outputs
    what you put in. Output has no style.

--- ddiep/form_test/js/scripts.js
+++ ddiep/form_test/js/scripts.js
@@ -1,8 +1,32 @@
 var form = document.forms.example;
 
-form.addEventListener("submit", action, false);
+form.addEventListener("submit", displayPerson, false);
 
-function action(event) {
+function displayPerson(event) {
 	event.preventDefault();
-	alert(form.txtbox.value);
+	console.log("submitted");
+	//form.name.value
+	
+	var Person = {
+		name: form.username.value,
+		secretData: form.secretData.value,
+		bio: form.bio.value,
+		gender: form.gender.value,
+		//limbs: form.limbs
+		car: form.cars.value
+	};
+
+	var l = [];
+
+	for(i = 0; i < form.limbs.length; i++) {
+		if(form.limbs[i].checked) {
+			l.push(form.limbs[i].value);
+		}
+	}
+
+	Person.limbs = l;
+
+	var html = "<p>My name is " + Person.name + ".</p><p>My secret data is " + Person.secretData + ".</p><p>Here's a bio about me... " + Person.bio + "</p><p>My gender is " + Person.gender + ".</p><p>I have these limbs: " + Person.limbs.join(", ") + ".</p><p>I want this car: " + Person.car + ".</p>";
+
+	$("#content-display").html(html);
 }
