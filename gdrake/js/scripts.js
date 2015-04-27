e613e9c4821a193920622b8c4dbf50bb7d556306
pogodrake <student@it515566.ad.ppsnet>
2015-04-27 10:25:18 -0700

    File Setup
    expanded everything out for fun time preparations

---
+++ gdrake/js/scripts.js
@@ -0,0 +1,18 @@
+function wordCountT()
+{
+// get the div 
+var outputDivId = document.getElementById('outputDiv');
+
+// write javaScript variable to the div
+outputDivId.innerHTML = document.myFormT.myText.value;
+}
+
+
+function wordCountTA()
+{
+// get the div 
+var outputDivId = document.getElementById('outputDiv');
+
+// write javaScript variable to the div
+outputDivId.innerHTML = document.myFormTA.myTextArea.value;
+}
