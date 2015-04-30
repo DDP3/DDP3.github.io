7fbe8d5d3eb7bc3a0eb4d62aff308698c2a27317
Dustin Diep <dustindiep0@gmail.com>
2015-04-30 13:37:51 -0700

    Completed basic crash course in scripts
    Index does nothing.

--- ddiep/object_cc/js/scripts.js
+++ ddiep/object_cc/js/scripts.js
@@ -1,6 +1,11 @@
+/*What are objects?*/
+
 //Objects are a very important part of coding. Javascript is considered an object oriented programming (OOP) language, meaning it uses objects.
 //Objects can be though of as a container, like an array, which stores relevant information and can interact with it. It can have variables and functions, but they're called properties and methods respectively.
 //But what can be an object? Well, pretty much anything that's a noun. A person, an animal, food, or, more practically, a user or application handler. Any set of related variables and functions can be grouped together into an object.
+
+
+/*How to make an object*/
 
 //I'm gonna make an object that's like a person. He'll be called Bob. You make an object like a variable, except you use curly brackets instead.
 //var objectName = { ...stuff in here... }
@@ -50,21 +55,85 @@ var name = Bob.name;
 Bob.sayName();
 Bob.changeGender("other");
 
-//An object constructor and a person being made with a constructor.
+
+/*Object constructors*/
+
+//The method is above is called making an object literal, where you make a whole object in one go.
+//Now, that's great an all for making a singular object, but what if we want to make a ton of objects instead?
+//Do we have to manually code each and every single person?
+//No! Objects can be grouped into different categories, called classes.
+//In this case, Bob would belong to the Person class, cause he's a person.
+//We use classes to provide a sort of framework for what an object is supposed to have,
+//then instantiate an instance of the object when we actually need one.
+
+//That sounds a little complicated, but hopefully it'll make more sense with an example.
+//Below is something called a constructor function. It's what we use to construct a new object.
+// *Please note that Javascript does not actually formally use classes, but it's still the same concept.*
+
+//This is the constructor function for the Person class. It takes some parameters such as name, gender, and age.
 
 var Person = function(name, gender, age) {
+
+	//Instead of the format from before where you define key:value pairs
+	//We use the "this" key word followed by whatever you want the property to be called, all using the dot format.
+	// this.propertyName
+	// *Remember, "this" refers to this object.
+
 	this.name = name;
 	this.gender = gender;
 	this.age = age;
+
+	//Methods are done similarly. Just use 
+	// this.methodName = function() { ...code here... }
+
 	this.sayName = function() {
 		console.log(this.name);
-	},
+	};
 	this.changeGender = function(newGender) {
 		gender = newGender;
 		console.log(name + "'s gender is now " + gender);
-	}
+	};
 }
 
+//Now we make a new Person object. I'm gonna call him Charles.
+//You use the format
+// var newObjectName = new Constructor( ...Properties... );
+
 var Charles = new Person("Charles", "male", 27);
+
+//We can access Charles' properties and methods just like Bob.
+
+var name2 = Charles.name;
 Charles.sayName();
 Charles.changeGender("female");
+
+
+/*Built-in Objects*/
+
+//Now javascript actually has some built-in functions. You've been using them without even knowing it!
+//Has this dot format looked kind of familiar?
+//That's cause you've been using it with the console object.
+
+console.log("I'm a built in function!");
+
+//The console.log function is a method that's built into the pre-existing console object.
+
+//You may also remember using the Math object. This has a bunch of built in methods for doing math.
+
+var random = Math.random();
+
+//There is also the Date object, which you can use for time management.
+
+var today = new Date();
+
+//You can get the full list here:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+// It's not super important to have memorized or anything, but it's pretty cool to know.
+
+
+/*Conclusion*/
+
+//Well, that's a crash course on Javascript Objects. This of course only scratches the surface.
+//There're still things like inheritance, prototyping, Object Oriented Design, etc. that I didn't get into (mostly cause I don't know much about them).
+//Feel free to research on your own.
+
+
