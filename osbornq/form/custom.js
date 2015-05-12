77a0961cc7357c571bd20f4708f7f419fbbbe24a
osbornq <qlosborn@gmail.com>
2015-05-12 13:09:01 -0700

    Update custom.js

--- osbornq/form/custom.js
+++ osbornq/form/custom.js
@@ -1,1 +1,11 @@
+var form = document.forms.theForm;
+form.addEventListener("submit",submitForm,false);
+function submitForm(event) {
+  event.preventDefault();
+  var cookie = {
+    favoriteCookie: form.cookieInput.value,
+  }
+  var cookieText = "<p>Your favorite kind of cookie is " + cookie.favoriteCookie + ".</p>"
+}
 
+document.getElementById("cookieAnswer").innerHTML = "cookieTest";
