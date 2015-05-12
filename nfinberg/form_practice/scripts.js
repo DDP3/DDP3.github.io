b8e51a5e1634a136d174680b47912e5a7f1a566f
Nick FInberg <nickfinberg@gmail.com>
2015-05-12 13:32:39 -0700

    Update scripts.js

--- nfinberg/form_practice/scripts.js
+++ nfinberg/form_practice/scripts.js
@@ -1,3 +1,3 @@
 function sellInformation() {
 			document.getElementById("informationSold").innerHTML = "<p>Thank you for submitting your information to all of the major crime organizations.</p>";
-		}
+		};
