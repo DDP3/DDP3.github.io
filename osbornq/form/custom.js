c7de0b34d6efe55e5a38ccb0f5ecfb6faf59a3e8
osbornq <qlosborn@gmail.com>
2015-05-12 13:18:26 -0700

    Update custom.js

--- osbornq/form/custom.js
+++ osbornq/form/custom.js
@@ -6,6 +6,17 @@ function submitForm(event) {
     favoriteCookie: form.cookieInput.value,
   }
   var cookieText = "<p>Your favorite kind of cookie is " + cookie.favoriteCookie + ".</p>";
+  
+  function dogBox {
+    if (document.getElementById("dog").checked) {
+      var dog= "dog";
+    }
+    else {
+      break;
+      console.log("No dog owned");
+    }
+  }
 }
 
 document.getElementById("cookieAnswer").innerHTML = cookieText;
+document.getElementById("petAnswer").innerHTML = "<p>You own the following pets: " + dog + cat + turtle + hamster + rabbit + other + ".</p>";
